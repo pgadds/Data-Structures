41d1e2f111ca80f4d42c8e545d30390d6d2d19a9
pgadds <60829558+pgadds@users.noreply.github.com>
2021-02-08 18:53:21 -0500

    Add files via upload

---
+++ Mazes and Stacks/src/Maze.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class Maze extends JFrame {
+
+    public Maze() {
+
+        this.add(new MazeGridPanel(5, 5));
+        this.setSize(800, 800);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Maze();
+    }
+}
